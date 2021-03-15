4837b236e3a7db7c1a60c7af3d2c22eb1cb5e9bb
Diego Kleinman <diegokleinman1@gmail.com>
2021-03-15 09:37:36 -0300

    create and use database

--- Ripio testing.sql
+++ Ripio testing.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `Accounts`
 --
-
+CREATE DATABASE Ripio testing;
+USE Ripio testing;
 DROP TABLE IF EXISTS `Accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
--- Ripio.sql
+++ Ripio.sql
@@ -17,8 +17,9 @@
 
 --
 -- Table structure for table `Accounts`
---
-
+--v
+CREATE DATABASE Ripio;
+USE Ripio;
 DROP TABLE IF EXISTS `Accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
